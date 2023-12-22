8bc8cd5160ea9f2c49367704b881672898f0b999
Benzazoua <billalben252@gmail.com>
2023-12-22 19:10:17 +0100

    initial the api.js file & working with api

---
+++ js/api.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export async function fetchData(url, successCallback, errorCallback) {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  } else {
+    const error = await response.json();
+    errorCallback && errorCallback(error);
+  }
+}
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import fetchData from "./api.js";
+
 // Add eventListener on multiple elements
 const addEventsOnElements = function ($elements, eventType, callback) {
   for (const $item of $elements) {
