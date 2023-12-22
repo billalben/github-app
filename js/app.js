ae590104a9b7eb87532c3ebbb6a3ba97e850ec2d
Benzazoua <billalben252@gmail.com>
2023-12-22 18:18:36 +0100

    Tab and tab content section

--- js/app.js
+++ js/app.js
@@ -27,3 +27,39 @@ $searchToggler.addEventListener("click", function () {
   this.setAttribute("aria-expanded", isExpanded);
   $searchField.focus();
 });
+
+// Tab Navigation
+const $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let [$lastActiveTabBtn, $lastActiveTabPanel] = [$tabBtns[0], $tabPanels[0]];
+
+addEventsOnElements($tabBtns, "click", function () {
+  $lastActiveTabBtn.setAttribute("aria-selected", false);
+  $lastActiveTabPanel.setAttribute("hidden", "");
+
+  this.setAttribute("aria-selected", true);
+  const $currentTabPanel = document.querySelector(
+    `#${this.getAttribute("aria-controls")}`
+  );
+  $currentTabPanel.removeAttribute("hidden");
+
+  $lastActiveTabBtn = this;
+  $lastActiveTabPanel = $currentTabPanel;
+});
+
+// Keyboard Accessibility For Tab Buttons
+addEventsOnElements($tabBtns, "keydown", function (event) {
+  const $nextElement = this.nextElementSibling;
+  const $previousElement = this.previousElementSibling;
+
+  if (event.key === "ArrowRight" && $nextElement) {
+    this.setAttribute("tabindex", "-1");
+    $nextElement.setAttribute("tabindex", "0");
+    $nextElement.focus();
+  } else if (event.key === "ArrowLeft" && $previousElement) {
+    this.setAttribute("tabindex", "-1");
+    $previousElement.setAttribute("tabindex", "0");
+    $previousElement.focus();
+  }
+});
